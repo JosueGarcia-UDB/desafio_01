15510670acf8450b5bbc91c5d0ba481d05044eb0
Josue Garcia <GE240098@alumno.udb.edu.sv>
2025-02-28 16:13:04 -0600

    Desafio avanzado

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState } from 'react';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ListaHorarios from "../components/ListaHorarios";
+import Information from "../components/Information"
+import FormularioCita from "../components/FormularioCita"
+import ResumenCitas from '../components/ResumenCitas';
+
+import "./globals.css"
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [citas, setCitas] = useState([]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+  const agregarCita = (nuevaCita) => {
+    setCitas([...citas, nuevaCita]);
+  };
+
+  const cancelarCita = (index) => {
+    const nuevasCitas = [...citas];
+    nuevasCitas.splice(index, 1);
+    setCitas(nuevasCitas);
+  };
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-grow-1">
+
+        {/*Sección de CardCitas y especialidades*/}
+        <section id="inicio" style={{ backgroundImage: "url(/images/background.png)", backgroundRepeat: "no-repeat", backgroundPositionX: "rigth" }}>
+          <Information />
+        </section>
+        {/*Sección de la lista de horarios*/}
+        <section id="disponibilidad" style={{ backgroundImage: "url(/images/background.png)", backgroundRepeat: "no-repeat" }}>
+          <ListaHorarios />
+        </section>
+        {/*Sección para reservar una cita*/}
+        <section id="reservar-cita">
+          <FormularioCita onAgregarCita={agregarCita} />
+        </section>
+        {/*Sección de Mis Citas*/}
+        <section id="mis-citas">
+          <ResumenCitas citas={citas} onCancelarCita={cancelarCita}/>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
