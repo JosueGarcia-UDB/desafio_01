ca26455e25696941177893992ad5408bb07a8dc0
Josue Garcia <GE240098@alumno.udb.edu.sv>
2025-02-28 16:11:53 -0600

    Add data directory

---
+++ src/app/data/especialidades.js
@@ -0,0 +1,42 @@
+export const especialidades = [
+  {
+    id: 1,
+    nombre: "Medicina General",
+    descripcion: "Atención médica primaria y diagnóstico general de enfermedades"
+  },
+  {
+    id: 2,
+    nombre: "Pediatría",
+    descripcion: "Especialidad médica enfocada en la salud de bebés, niños y adolescentes"
+  },
+  {
+    id: 3,
+    nombre: "Ginecología",
+    descripcion: "Atención especializada en la salud femenina y sistema reproductivo"
+  },
+  {
+    id: 4,
+    nombre: "Dermatología",
+    descripcion: "Diagnóstico y tratamiento de condiciones de la piel, cabello y uñas"
+  },
+  {
+    id: 5,
+    nombre: "Cardiología",
+    descripcion: "Especialidad centrada en el corazón y sistema cardiovascular"
+  },
+  {
+    id: 6,
+    nombre: "Traumatología",
+    descripcion: "Tratamiento de lesiones y enfermedades del sistema músculo-esquelético"
+  },
+  {
+    id: 7,
+    nombre: "Oftalmología",
+    descripcion: "Diagnóstico y tratamiento de enfermedades de los ojos"
+  },
+  {
+    id: 8,
+    nombre: "Nutrición",
+    descripcion: "Asesoramiento en alimentación saludable y planes nutricionales personalizados"
+  }
+]
